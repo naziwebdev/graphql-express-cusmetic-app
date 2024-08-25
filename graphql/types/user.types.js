11985b8cf1ed62d1b81563d8f4433d9b02d0348b
nazi <nazipaeez777@gmail.com>
2024-08-25 22:45:00 +0330

    Develope user types gqaphql

---
+++ graphql/types/user.types.js
@@ -0,0 +1,17 @@
+exports.User = `
+type User {
+_id:ID
+username:String
+email:String
+phone:String
+password:String
+role:String
+}
+`;
+
+exports.Auth = `
+type Auth {
+token:String,
+user:User
+}
+`;

1e4ba616af347ed32194f666570ea0abe7acaf75
nazi <nazipaeez777@gmail.com>
2024-08-26 22:18:25 +0330

    Create order validator

---
+++ graphql/resolvers/order.resolver.js
@@ -0,0 +1,14 @@
+const OrderModel = require('../../models/Order')
+
+
+module.exports = {
+    createOrder : async (_,args) => {
+        try {
+
+
+            
+        } catch (error) {
+            throw new Error(error)
+        }
+    }
+}
---
+++ validators/order.validator.js
@@ -0,0 +1,13 @@
+const yup = require("yup");
+
+exports.orderValidator = yup.object({
+  cusmetic: yup
+    .string()
+    .matches(/^[0-9a-fA-F]{24}$/, "id is invalid")
+    .required("this field is required"),
+  user: yup
+    .string()
+    .matches(/^[0-9a-fA-F]{24}$/, "id is invalid")
+    .required("this field is required"),
+  count: yup.number().required("this field is required"),
+});
